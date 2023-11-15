ff244ea6c68b265c1ffd74a4b7c2af56d883fa27
Youxu Chen <cyx1227@mail.ustc.edu.cn>
2023-11-15 09:51:07 +0800

    Update makefile to generate a executable file for each source file

--- src/sample05-transformation/test.cpp
+++ src/sample05-transformation/test.cpp
@@ -1,33 +1,33 @@
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// // (1,1) vector
-// // translate (0.5, -0.5)
-// // result --> (1.5, 0.5)
+// (1,1) vector
+// translate (0.5, -0.5)
+// result --> (1.5, 0.5)
 
-// int main() {
-//     glm::vec4 vec(1.0f, 1.0f, 0.0f, 1.0f);
-//     glm::mat4 trans = glm::mat4(1.0f);
+int main() {
+    glm::vec4 vec(1.0f, 1.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
     
-//     for (int i = 0; i < 4; i++) {
-//         for (int j = 0; j < 4; j++) {
-//             cout << trans[i][j] << "\t";
-//         }
-//         cout << endl;
-//     }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << trans[i][j] << "\t";
+        }
+        cout << endl;
+    }
 
-//     trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 
-//     for (int i = 0; i < 4; i++) {
-//         for (int j = 0; j < 4; j++) {
-//             cout << trans[j][i] << "\t";
-//         }
-//         cout << endl;
-//     }
-//     vec = trans * vec;
-//     std::cout << vec.x << vec.y << vec.z << std::endl;
-// }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << trans[j][i] << "\t";
+        }
+        cout << endl;
+    }
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+}
